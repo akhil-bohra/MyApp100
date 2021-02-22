b9a9ca1135531fa6a0488d074093689c1399826a
akhil-bohra <akhilbohra@gmail.com>
2021-02-22 19:13:00 +0900

    converted into maven project and tested it - commit 1

---
+++ MyApp100/src/com/ofss/TestProduct.java
@@ -0,0 +1,16 @@
+package com.ofss;
+
+public class TestProduct {
+
+	// a class with the main method serves as an entry to the application
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Stock st1=new Stock(100, "ORCL", 123.45);
+		System.out.println(st1);
+		System.out.println("123");
+
+
+	}
+
+}
